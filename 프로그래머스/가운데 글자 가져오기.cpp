c7e7643e59b8f9f9a234e0970c74203c0276bce2
YuJeong <qordbwjd96@gmail.com>
2020-06-04 03:59:26 +0900

    200603

---
+++ 프로그래머스/가운데 글자 가져오기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int len = s.length();
+    
+    if(len%2==0){
+        answer = s.substr(len/2-1,2);
+    }else{
+        answer = s.substr(len/2,1);
+    }
+    
+    return answer;
+}
