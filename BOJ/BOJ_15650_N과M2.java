3a77e709bba01e0e90e8fec4b6f6e683493c3027
YuJeong <qordbwjd96@gmail.com>
2021-05-09 00:31:30 +0900

    [BOJ/백트래킹] 15650_N과 M (2)

---
+++ BOJ/BOJ_15650_N과M2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15650_N과M2 {
+	static int N,M;
+	static int[] arr;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		visited = new boolean[N];
+		
+		combination(0);
+	}
+
+	private static void combination(int depth) {
+		if(depth==M) {
+			print();
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i+1;
+				combination(depth+1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	private static void print() {
+		boolean flag = true;
+		for(int i=0; i<M-1; i++) {
+			if(!(arr[i]<arr[i+1])) flag = false; 
+		}
+		
+		if(flag) {
+			for(int i=0; i<M; i++) {
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
