9aeed4585249d828740aaf5820f6e3d5ecfe5885
YuJeong <qordbwjd96@gmail.com>
2021-05-27 00:35:02 +0900

    [Programmers/수학] 3진법 뒤집기

--- programmers/삼진법뒤집기.java
+++ programmers/삼진법뒤집기.java
@@ -1,35 +1,47 @@
 
 public class 삼진법뒤집기 {
 	public static int solution(int n) {
-        int answer = 0;
         boolean flag = true;
-        int i = 0;
-        String ans = "";
+        int x = 0;
+        int y = 0;
+        String three = "";
+        int answer = 0;
         
         // 10진법 -> 3진법
         while(flag) {	
-        	if(Math.pow(3, i) > n) {
+        	if(Math.pow(3, x) > n) {
         		flag = false;
         	}else {
-        		i++;
+        		x++;
         	}
         }
         
-        while(!(n==0 && i==0)) {
-        	String s = Integer.toString((int) (n/(Math.pow(3, i-1))));
-        	ans += s;
-        	n %= (int) (Math.pow(3, i-1));
-        	i--;
+        y = x;
+        
+        while(!(n==0 && x==0)) {
+        	String s = Integer.toString((int) (n/(Math.pow(3, x-1))));
+        	three += s;
+        	n %= (int) (Math.pow(3, x-1));
+        	x--;
         }
         
-        answer = Integer.parseInt(ans);
-        
+        // 뒤집기
+        StringBuilder sb = new StringBuilder(three);
+		String reverseSb = sb.reverse().toString();
+		String[] arr = reverseSb.split("");
+		
+		// 3진법 -> 10진법
+		for(int i=0; i<arr.length; i++) {
+			int tmp = (int) (Integer.parseInt(arr[i]) * (Math.pow(3, y-1)));
+			answer += tmp;
+			y--;
+		}
+		
         return answer;
     }
 	
 	
 	public static void main(String[] args) {
-		
 		System.out.println(solution(45));
 	}
 }
