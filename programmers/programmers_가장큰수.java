183478297ab9f60013386ad3d566936f05c6c9af
YuJeong <qordbwjd96@gmail.com>
2021-06-02 01:11:01 +0900

    [programmers/정렬] 가장 큰 수

--- programmers/programmers_가장큰수.java
+++ programmers/programmers_가장큰수.java
@@ -1,20 +1,41 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Comparator;
 
+// 앞자리수를 다 빼내어서 정렬한다음, 
+// 한자리수 일 경우 바로 붙이기, 두 자리 수 일 경우 그 뒤의 숫자들을 비교 36>31 이런 식으로 늘려나가서 순서대로 붙이기라는 생각
+
+// 앞 수와 뒤 수를 붙여서 만들어 본 뒤, 더  큰 수를 배치
 public class programmers_가장큰수 {
 	public static String solution(int[] numbers) {
-        String answer = "";
+
+		// 숫자를 문자열로 변환
+		String[] result = new String[numbers.length];
         
-        HashMap<Integer,String> map = new HashMap<>();
+		for (int i = 0; i < numbers.length; i++) {
+			result[i] = String.valueOf(numbers[i]);
+		}
+
+		// 정렬
+		Arrays.sort(result, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return ((o2 + o1).compareTo(o1 + o2));
+			}
+		});
+
+		// 0만 여러개 있는 배열의 경우 하나의 0만 리턴
+		if(result[0].equals("0")) {
+			return "0";
+		}
+		
+		String answer = "";
+		// 정렬된 문자 하나로 합치기
+		for (String a : result) {
+			answer += a;
+		}
         
-        for(int i=0; i<numbers.length; i++) {
-        	String s = Integer.toString(numbers[i]);
-            s = s.split(0,1);
-            int n = Integer.parseInt(s);
-            map.add(new HashMap(i, n));
-        }
-        
-        return answer;
-    }
+		return answer;
+	}
 	
 	public static void main(String[] args) {
 		int[] numbers = {6, 10, 2};
