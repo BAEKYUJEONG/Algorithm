bf4557f46bd853cede2422cfad1b47c92b4ac574
YuJeong <qordbwjd96@gmail.com>
2021-06-11 10:57:49 +0900

    [BOJ/BFS] 2206_벽 부수고 이동하기

--- BOJ/BOJ_2206_벽부수고이동하기.java
+++ BOJ/BOJ_2206_벽부수고이동하기.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,8 +11,8 @@ public class BOJ_2206_벽부수고이동하기 {
 	static boolean[][] visited;
 	static int[] dx = {0,0,-1,1};
 	static int[] dy = {-1,1,0,0};
-	static Queue<Point> queue = new LinkedList<Point>();
-	static int answer = 0;
+	static Queue<Place> queue = new LinkedList<Place>();
+	static int answer = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,17 +31,57 @@ public class BOJ_2206_벽부수고이동하기 {
 			}
 		}
 		
-		queue.add(new Point(0,0));
+		queue.add(new Place(0,0,1,0));
+		visited[0][0] = true;
+		
 		bfs();
 		
+		if(answer == Integer.MAX_VALUE) answer = -1;
+		
+		System.out.println(answer);
 	}
 
 	private static void bfs() {
 		while(!queue.isEmpty()) {
-			Point p = queue.poll();
-			int r = p.x;
-			int c = p.y;
 			
+			Place p = queue.poll();
+			int r = p.r;
+			int c = p.c;
+			int distance = p.distance;
+			int wall = p.wall;
+			
+			if(r==N-1 && c==M-1) {
+				answer = Math.min(distance, answer);
+			}
+			
+			for(int i=0; i<4; i++) {
+				int nr = r+dx[i];
+				int nc = c+dy[i];
+				
+				if(nr>=0 && nr<N && nc>=0 && nc<M) {
+					if(!visited[nr][nc]) {
+						queue.add(new Place(nr, nc, distance+1, wall));
+						visited[nr][nc] = true;
+					}else if(wall==0) {
+						queue.add(new Place(nr, nc, distance+1, wall+1));
+						visited[nr][nc] = true;
+					}
+				}
+			}
+		}
+	}
+	
+	static class Place {
+		int r;
+		int c;
+		int distance;
+		int wall;
+		
+		public Place(int r, int c, int distance, int wall) {
+			this.r = r;
+			this.c = c;
+			this.distance = distance;
+			this.wall = wall;
 		}
 	}
 }
