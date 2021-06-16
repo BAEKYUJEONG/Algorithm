e94a6ff5414f3046b64b73a270e4e9782260c70a
YuJeong <qordbwjd96@gmail.com>
2021-06-16 11:12:48 +0900

    [BOJ/수학] 10869_사칙연산

---
+++ BOJ/BOJ_10869_사칙연산.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10869_사칙연산 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+	}
+}
