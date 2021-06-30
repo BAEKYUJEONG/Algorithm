773f3c19f6f2cc35e6df8f760f9346560ca25312
YuJeong <qordbwjd96@gmail.com>
2021-06-30 15:21:40 +0900

    [programmers/구현] 제일 작은 수 제거하기

---
+++ programmers/programmers_제일작은수제거하기.java
@@ -0,0 +1,34 @@
+class Solution {
+    static int index;
+
+    public int[] solution(int[] arr) {
+        int[] answer;
+
+        if(arr.length==1){
+            answer = new int[1];
+            answer[0] = -1;
+        }else{
+            answer = new int[arr.length-1];
+            int min = Integer.MAX_VALUE;
+            index = 0;
+
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]<min){
+                    min = arr[i];
+                    index = i;
+                }
+            }
+
+            if(index==0){
+                System.arraycopy(arr, 1, answer, 0, answer.length);
+            }else if(index==arr.length-1){
+                System.arraycopy(arr, 0, answer, 0, answer.length);
+            }else{
+                System.arraycopy(arr, 0, answer, 0, index);
+                System.arraycopy(arr, index+1, answer, index, answer.length-index);
+            }
+        }
+
+        return answer;
+    }
+}
