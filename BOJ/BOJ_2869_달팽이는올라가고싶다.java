35fa5afd91725dda1a728bf1269f0e2c721fcd7a
YuJeong <qordbwjd96@gmail.com>
2021-08-22 11:41:55 +0900

    [BOJ/구현] 2869_달팽이는 올라가고 싶다

---
+++ BOJ/BOJ_2869_달팽이는올라가고싶다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2869_달팽이는올라가고싶다 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+		
+		int day = (v-a) / (a-b) + 1;
+		
+		if((v-a) % (a-b) != 0) {
+			day++;
+		}
+		
+		System.out.println(day);
+	}
+}
