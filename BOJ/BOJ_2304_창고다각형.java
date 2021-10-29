8dc88e10c878fd9221f2a32d6427421afb47615f
YuJeong <qordbwjd96@gmail.com>
2021-10-29 17:55:54 +0900

    [BOJ/브루트포스] 2304_창고 다각형

--- BOJ/BOJ_2304_창고다각형.java
+++ BOJ/BOJ_2304_창고다각형.java
@@ -5,42 +5,42 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class BOJ_1152_단어의개수 {
-	
+public class BOJ_2304_창고다각형 {
+
 	static boolean[][] box = new boolean[1000][1000];
 	static ArrayList<Bar> bars = new ArrayList<Bar>();
 	static int sum = 0;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int L = Integer.parseInt(st.nextToken());
 			int H = Integer.parseInt(st.nextToken());
-			
+
 			bars.add(new Bar(L, H));
 		}
-		
+
 		Collections.sort(bars, Collections.reverseOrder());
-		
+
 		Bar barTop = bars.remove(0);
 		int middle = barTop.L;
 		int top = barTop.H;
-		
+
 		sum += top;
-		
+
 		int left = middle;
 		int right = middle;
-		
+
 		while(!bars.isEmpty()) {
 			Bar bar1 = bars.remove(0);
 			int bL = bar1.L;
 			int bH = bar1.H;
-			
+
 			if(bL < middle) {
 				if(left > bL) {
 					sum += (left-bL) * bH;
@@ -53,7 +53,7 @@ public class BOJ_1152_단어의개수 {
 				}
 			}
 		}
-		
+
 		System.out.println(sum);
 	}
 
@@ -62,7 +62,7 @@ public class BOJ_1152_단어의개수 {
 class Bar implements Comparable<Bar> {
 	int L;
 	int H;
-	
+
 	public Bar(int L, int H) {
 		this.L = L;
 		this.H = H;
@@ -70,7 +70,7 @@ class Bar implements Comparable<Bar> {
 
 	@Override
 	public int compareTo(Bar bar) {
-		
+
 		if(this.H < bar.H) {
 			return -1;
 		} else if(this.H == bar.H) {
@@ -78,7 +78,7 @@ class Bar implements Comparable<Bar> {
 		} else {
 			return 1;
 		}
-		
+
 	}
-	
-}
+
+} 
