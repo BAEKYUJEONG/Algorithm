93e9588d7c2af4fe72825488af3bf74438cf215c
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-03-17 21:20:28 +0900

    [programmers/구현] 평균 구하기

---
+++ programmers/programmers_평균구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for(int n: arr) {
+            answer += n;
+        }
+
+        return answer / arr.length;
+    }
+}
