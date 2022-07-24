7977a93d5932e2247d5b748a69c410591483f619
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-07-24 23:42:24 +0900

    [BOJ/구현] 3052_나머지

---
+++ BOJ/BOJ_3052_나머지.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> h = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			h.add(Integer.parseInt(br.readLine()) % 42);
+		}
+		
+		System.out.print(h.size());
+	}
+}
