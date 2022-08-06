452ab7f3b2ebef59da74f64016a0188dae52b875
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-08-06 10:18:22 +0900

    [programmers/문자열] 숫자 문자열과 영단어

--- Swift/Algorithm/Algorithm/Programmers.swift
+++ Swift/Algorithm/Algorithm/Programmers.swift
@@ -9,6 +9,16 @@ import Foundation
 
 final class programmers {
     
+    /// 숫자 문자열과 영단어
+    func solution_81301(_ s:String) -> Int {
+        let arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        var str = s
+        for i in 0..<arr.count {
+            str = str.replacingOccurrences(of: arr[i], with: String(i))
+        }
+        return Int(str)!
+    }
+    
     /// 음양 더하기
     func solution_76501(_ absolutes:[Int], _ signs:[Bool]) -> Int {
         var answer: Int = 0
